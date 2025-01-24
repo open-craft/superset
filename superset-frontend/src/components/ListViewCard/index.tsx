fc481232952d9e95e3ea3814bc31012f4d6845c5
Jillian Vogel <jill@opencraft.com>
2025-01-24 19:57:10 +1030

    temp: proof of concept for asset translations

--- superset-frontend/src/components/ListViewCard/index.tsx
+++ superset-frontend/src/components/ListViewCard/index.tsx
@@ -17,7 +17,7 @@
  * under the License.
  */
 import React from 'react';
-import { styled, useTheme } from '@superset-ui/core';
+import { styled, t, useTheme } from '@superset-ui/core';
 import { Skeleton, AntdCard } from 'src/components';
 import { Tooltip } from 'src/components/Tooltip';
 import ImageLoader, { BackgroundPosition } from './ImageLoader';
@@ -265,7 +265,7 @@ function ListViewCard({
                         />{' '}
                       </>
                     )}
-                    {title}
+                    {t(title)}
                   </TitleLink>
                 </Tooltip>
                 {titleRight && <TitleRight>{titleRight}</TitleRight>}
